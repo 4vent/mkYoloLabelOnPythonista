1d30815f779a0920130250589cf97a7d753195a7
Avent <44436695+abeckent@users.noreply.github.com>
2022-03-20 03:36:06 +0900

    modify

--- modules/get_string_width.py
+++ modules/get_string_width.py
@@ -6,8 +6,8 @@ def getStringWidth(str=''):
     width = 0
     half = ['H', 'Na', 'N']
     for c in str:
-        uc = ord(c)
-        if unicodedata.east_asian_width(uc) in half:
+        # uc = ord(c)
+        if unicodedata.east_asian_width(c) in half:
             width += 1
         else:
             width += 2
