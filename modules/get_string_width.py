b3ce14ac9deee47d81bd5343d6e2fbbfb1ae73b2
Avent <44436695+abeckent@users.noreply.github.com>
2022-03-20 03:44:15 +0900

    unicodeの取り扱いをバージョンごとに定義

--- modules/get_string_width.py
+++ modules/get_string_width.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import unicodedata
 
 def getStringWidth(str=''):
     width = 0
     half = ['H', 'Na', 'N']
     for c in str:
-        # uc = ord(c)
-        if unicodedata.east_asian_width(c) in half:
+        if sys.version_info.major < 3:
+            uc = unicode(c)
+        else:
+            uc = c
+        if unicodedata.east_asian_width(uc) in half:
             width += 1
         else:
             width += 2
     return width
+
+# debug
+
+print(getStringWidth('あほ'))
